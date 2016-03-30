02e8d5a9b4fd63c7cd4a88c37e50324f4c423a3b
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2016-03-30 12:03:49 +0700

    Updated FAKE build
    Looks a bit better now..

--- build.fsx
+++ build.fsx
@@ -1,36 +1,69 @@
-// include Fake lib
-
+// --------------------------------------------------------------------------------------
+// FAKE build script 
+// --------------------------------------------------------------------------------------
 
 #r @"tools\FAKE\tools\FakeLib.dll"
-
 open Fake
+open Fake.AssemblyInfoFile
 
+// --------------------------------------------------------------------------------------
+// Information about the project to be used at NuGet and in AssemblyInfo files
+// --------------------------------------------------------------------------------------
+let project = "Azure Logic App Monitoring"
+let authors = ["Kevin Bronsdijk"]
+let summary = "Azure Logic App Monitoring"
+let version = "1.0.0.1"
+let description = """
+Solution which will track failed Logic Apps workflow runs within Azure Applications Insight.
+"""
+let notes = ""
+let tags = "LogicApps C# API Monitoring Alerts"
+let gitHome = "https://github.com/Kevin-Bronsdijk"
+let gitName = "LogicApps-Monitoring"
 
+// --------------------------------------------------------------------------------------
+// Build script 
+// --------------------------------------------------------------------------------------
 
-// Properties
 let buildDir = "./output/"
 
-// Targets
+// --------------------------------------------------------------------------------------
+
 Target "Clean" (fun _ ->
  CleanDir buildDir
 )
 
-Target "BuildApp" (fun _ ->
+// --------------------------------------------------------------------------------------
+
+Target "AssemblyInfo" (fun _ ->
+    let attributes =
+        [ 
+            Attribute.Title project
+            Attribute.Product project
+            Attribute.Description summary
+            Attribute.Version version
+            Attribute.FileVersion version
+        ]
+
+    CreateCSharpAssemblyInfo "src/LogicAppsMonitoring.Logic/Properties/AssemblyInfo.cs" attributes
+    CreateCSharpAssemblyInfo "src/LogicAppsMonitoring.WebJob/Properties/AssemblyInfo.cs" attributes
+)
+
+// --------------------------------------------------------------------------------------
+
+Target "Build" (fun _ ->
  !! "src/*.sln"
  |> MSBuildRelease buildDir "Build"
  |> Log "AppBuild-Output: "
 )
 
-Target "Default" (fun _ ->
- trace "LogicAppsMonitoring Build started"
-)
+// --------------------------------------------------------------------------------------
 
-// Dependencies
+Target "All" DoNothing
+
 "Clean"
-  ==> "BuildApp"
-  ==> "Default"
+  ==> "AssemblyInfo"
+  ==> "Build"
+  ==> "All"
 
-// start build
-RunTargetOrDefault "Default"
-
-// ToDo: Packaging and Deploying Azure WebJobs
+RunTargetOrDefault "All"
