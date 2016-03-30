d84960edbcebafbdd84a4458003694f15b42f1f8
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2016-03-30 11:51:42 +0700

    Cleanup Assembly Info
    Removed Assembly Info, will be generated by FAKE

--- src/LogicAppsMonitoring.WebJob/Properties/AssemblyInfo.cs
+++ src/LogicAppsMonitoring.WebJob/Properties/AssemblyInfo.cs
@@ -2,34 +2,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Azure Logic App Monitoring WebJob")]
 [assembly: AssemblyDescription("Azure Logic App Monitoring WebJob")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Azure Logic App Monitoring")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ba48a9bb-164a-4206-86e8-0e0174ad29a5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
